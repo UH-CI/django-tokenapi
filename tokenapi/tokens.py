e2f124b9a5dffe446c6be40c55169968305cd366
davidls <davidls@hawaii.edu>
2015-07-13 13:15:54 -1000

    Modified token code so that we can have tokens not expire.  Since we are not using the django password (using CAS), we can repurpose the password field to contain an invalid password that we can use to hash the key.  It also has an additional salt on the key.. it is currently passed along with the token, but this salt probably isn't needed and will possibly be removed in the future.  The hash is also base64 encoded in a safe manner to be passed in the url

--- tokenapi/tokens.py
+++ tokenapi/tokens.py
@@ -5,6 +5,8 @@ from django.conf import settings
 from django.utils.http import int_to_base36, base36_to_int
 from django.utils.crypto import constant_time_compare, salted_hmac
 from django.utils import six
+from django.contrib.auth.hashers import make_password
+import base64
 
 
 class PasswordResetTokenGenerator(object):
@@ -17,6 +19,7 @@ class PasswordResetTokenGenerator(object):
         Returns a token that can be used once to do a password reset
         for the given user.
         """
+        user.password = None
         return self._make_token_with_timestamp(user, self._num_days(self._today()))
 
     def check_token(self, user, token):
@@ -25,7 +28,7 @@ class PasswordResetTokenGenerator(object):
         """
         # Parse the token
         try:
-            ts_b36, hash = token.split("-")
+            ts_b36, key_salt, hashval = token.split("-")
         except ValueError:
             return False
 
@@ -35,31 +38,45 @@ class PasswordResetTokenGenerator(object):
             return False
 
         # Check that the timestamp/uid has not been tampered with
-        if not constant_time_compare(self._make_token_with_timestamp(user, ts), token):
+        if not constant_time_compare(self._make_token_with_timestamp(user, ts, key_salt), token):
             return False
 
-        # Check the timestamp is within limit
-        if (self._num_days(self._today()) - ts) > getattr(settings, 'TOKEN_TIMEOUT_DAYS', 7):
-            return False
-
+        expiretime = getattr(settings, 'TOKEN_TIMEOUT_DAYS', 7)
+        if expiretime:
+            expiretime = int(expiretime)
+            if expiretime > 0:
+                # Check the timestamp is within limit
+                if (self._num_days(self._today()) - ts) > expiretime:
+                    return False
         return True
 
-    def _make_token_with_timestamp(self, user, timestamp):
+    def _make_token_with_timestamp(self, user, timestamp, key_salt = None):
         # timestamp is number of days since 2001-1-1.  Converted to
         # base 36, this gives us a 3 digit string until about 2121
         ts_b36 = int_to_base36(timestamp)
 
-        key_salt = "tokenapi.tokens.PasswordResetTokenGenerator"
+        #key_salt =  getattr(settings, 'TOKEN_SALT', "tokenapi.tokens.PasswordResetTokenGenerator")
+        if not user.password: 
+            user.password = make_password(None)
+            user.save()
+
+        if not key_salt:
+            key_salt = make_password(None)
+            key_salt = salted_hmac(six.text_type(user.pk), key_salt).hexdigest()
 
         value = (six.text_type(user.pk) + user.password + six.text_type(timestamp))
-        hash = salted_hmac(key_salt, value).hexdigest()[::2]
-        return "%s-%s" % (ts_b36, hash)
+        #hash = salted_hmac(key_salt, value).hexdigest()[::2]
+        hashval = salted_hmac(key_salt, value).hexdigest()        
+        return base64.urlsafe_b64encode("%s-%s-%s" % (ts_b36, key_salt, hashval))
+
 
     def _num_days(self, dt):
         return (dt - date(2001, 1, 1)).days
 
+
     def _today(self):
         # Used for mocking in tests
         return date.today()
+
 
 token_generator = PasswordResetTokenGenerator()
